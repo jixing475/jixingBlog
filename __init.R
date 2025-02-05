fd8802cf3d83516432ffa0e596666f31cc24588d
jixing475 <jixing475@gmail.com>
2025-02-05 21:50:31 +0800

    Update 公众号的文章

---
+++ __init.R
@@ -0,0 +1,12 @@
+distill::create_post("Using-Active-Learning-in-a-3D-Similarity-Search")
+
+usethis::edit_file("_posts/2024-04-09-using-active-learning-in-a-3d-similarity-search.Rmd")
+
+# 注意设置不要重新编译
+# 注意是否需要 bib
+# 注意博客显示的是标题, 而不是文件名
+# https://jixing.netlify.app/
+
+
+distill::create_post("Kinase-Bench-Comprehensive-benchmarking-tool-for-improving-kinase-inhibitor-selectivity")
+
